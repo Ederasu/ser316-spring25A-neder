bb3279175080147a39b26b1220291d608eb08164
Ederasu <Nicolas.J.Eder@gmail.com>
2025-02-12 23:46:11 -0600

    adjusting GuessTest

--- src/test/java/GuessTests.java
+++ src/test/java/GuessTests.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class GuessTest { //SER316 TASK 2 SPOT-BUGS FIX
+public class GuessTests { //SER316 TASK 2 SPOT-BUGS FIX
 
     // Tests when a single letter guesses is within the correct word
     @Test
